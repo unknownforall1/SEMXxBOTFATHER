db8f838949e7d8028d7d0d4e6ba9cf8b5a175bb8
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-12-18 12:14:38 +0530

    Update bot.py

--- SemxXSpam/plugins/bot.py
+++ SemxXSpam/plugins/bot.py
@@ -25,7 +25,7 @@ async def ping(e):
         event = await e.reply("𝙋𝙤𝙣𝙜!", parse_mode=None, link_preview=None )
         end = datetime.now()
         ms = (end-start).microseconds / 1000
-        await event.edit(f"🎉 BHOSDIWALA ✘SPAM 🤖!\n✘`{ms}` ms\n 😈𝙍𝙀𝘼𝘿𝙔 𝙁𝙊𝙍 𝙎𝙀𝙈𝙓🤧")
+        await event.edit(f"🎉  𝙋𝙤𝙣𝙜 🤖!\n✘`{ms}` ms\n 😈𝙍𝙀𝘼𝘿𝙔 𝙁𝙊𝙍 𝙎𝙀𝙓🤧")
         
         
 
